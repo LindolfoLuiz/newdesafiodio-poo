b686e491e10bfa970fba7f57fb792b2834e38c02
LindolfoLuiz <lindolfofacu@hotmail.com>
2023-02-10 17:03:51 -0300

    desafio dio lindolfo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("Lindolfo", 23);
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Idade: " + pessoa.getIdade());
+
+        pessoa.setNome("Maria");
+        pessoa.setIdade(30);
+
+        System.out.println("\nNome atualizado: " + pessoa.getNome());
+        System.out.println("Idade atualizada: " + pessoa.getIdade());
+    }
+}
